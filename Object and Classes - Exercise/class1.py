c564586ce22a1f00b6cfea4caebbd2692dd216ca
Zhaklin <zhaklin.vargova@gmail.com>
2023-01-20 15:21:16 +0200

    Added task Class

--- Object and Classes - Exercise/class1.py
+++ Object and Classes - Exercise/class1.py
@@ -1,25 +1,22 @@
 class Class:
     __students_count = 22
-    students = list()
-    grades = list()
 
     def __init__(self, name):
         self.name = name
-        self.current_count = 0
-        self.average = 0
+        self.students = list()
+        self.grades = list()
 
     def add_student(self, name: str, grade: float):
-        if self.__students_count > self.current_count:
-            self.students.append(name)
-            self.grades.append(grade)
-            self.current_count += 1
+        if self.__students_count > len(self.students):
+            self.students.append(str(name))
+            self.grades.append(float(grade))
 
     def get_average_grade(self):
-        self.average = sum(self.grades) / len(self.grades)
-        return f"{self.average}"
+        average = f"{(sum(self.grades) / len(self.grades)):.2f}"
+        return float(average)
 
     def __repr__(self):
-        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.average:.2f}"
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {Class.get_average_grade(self)}"
 
 
 a_class = Class("11B")
@@ -27,4 +24,3 @@ a_class.add_student("Peter", 4.80)
 a_class.add_student("George", 6.00)
 a_class.add_student("Amy", 3.50)
 print(a_class)
-
