e69d16839cb82338c3b568798b6deb7dff707d70
Zhaklin <zhaklin.vargova@gmail.com>
2023-01-27 00:33:12 +0200

    Added task odd_occurrences

---
+++ Dictionaries/odd_occurrences.py
@@ -0,0 +1,12 @@
+input_data = input().split()
+my_dict = {}
+
+for word in input_data:
+    word_lower = word.lower()
+    if word_lower not in my_dict:
+        my_dict[word_lower] = 0
+    my_dict[word_lower] += 1
+
+for key, value in my_dict.items():
+    if value % 2 != 0:
+        print(key, end=" ")
