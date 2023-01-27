9450663f1c90880ded7f1e90ccd4db925eab26f9
Zhaklin <zhaklin.vargova@gmail.com>
2023-01-27 23:02:40 +0200

    Added task "Capitals"

---
+++ Dictionaries/capitals.py
@@ -0,0 +1,11 @@
+# Write a program that receives country names on the first line, separated by comma and space ", ",
+# and their corresponding capital cities on the second line (again separated by comma and space ", ").
+# Print each country with their capital on a separate line in the following format: "{country} -> {capital}".
+
+countries = input().split(", ")
+capitals = input().split(", ")
+
+new_dict = dict(zip(countries,capitals))
+
+for country, capital in new_dict.items():
+    print(f"{country} -> {capital}")
